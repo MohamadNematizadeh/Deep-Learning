ec53b271c8c108d95a4f02cd72d4ea4167d813cf
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-12-30 14:18:57 +0330

    Update Audi_Classifier.py

--- 7.9.Audio Classification/Audi_Classifier.py
+++ 7.9.Audio Classification/Audi_Classifier.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
-# import librosa
+import librosa
+from model import M4
 
 SAMPLE_RATE = 48000
 def resample_audio(path):
@@ -13,60 +14,31 @@ def resample_audio(path):
 
 class Audio_Classifier(tf.keras.Model):
     def __init__(self):
-        labels = ['Parsa','Azra Khedadmand','Davood Fazeli','Javad Nematollahi','Parisa Baqerzade','kiana jhnshid','Maryam Saeedi','Matin Ghorbani','Shima Bazzazan','Mohammad','Nima','Omid nomiri','Khadijeh Valipour','Abdollah Ramezani','Rezaie','Sajedeh Gharabadiyan','Mohammad_prf']
-
-    def train(model,train_data,validation_data):
-        model.compile(
-            optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-            loss='categorical_crossentropy',
-            metrics=['accuracy'])
+        labels = ['abdollah', 'azra', 'davood', 'javad', 'kiana', 'matin', 'mohamad', 'mohamadd', 'mona', 'nima', 'omid', 'parisa', 'parsa', 'saeedi', 'sajedeh', 'shima', 'tara', 'valipour']
+    def train(train_data,validation_data):
+        model = M4
         model.fit(train_data, validation_data=validation_data, epochs=30)
         
-    def predict(train_data,validation_data,model):  
-        model.fit(train_data, validation_data=validation_data, epochs=30)
-
     def preprocess(wav_filename):
         asd = resample_audio(wav_filename)
-        input_data = asd.reshape(1,48000,1) 
+        desired_length = 48000
+        resized_waveform = librosa.util.fix_length(asd, size = desired_length)
+
+        input_data = np.expand_dims(resized_waveform, axis=-1)
+        input_data = np.expand_dims(input_data, axis=0)
         return(input_data)       
   
     def postprocess(model,input_data):  
       preds = np.argmax(model(input_data))
       return(preds)
-      
-    def create_model(train_data,validation_data,output=17,channel=32,):
-        Conv1=tf.keras.layers.Conv1D(channel, kernel_size=80, activation='relu', strides=16, input_shape=(48000, 1)),
-        MaxPooling1=tf.keras.layers.MaxPooling1D(4),
-        Conv2=tf.keras.layers.Conv1D(channel, kernel_size=3, activation='relu'),
-        MaxPooling2=tf.keras.layers.MaxPooling1D(4),
-        Conv3=tf.keras.layers.Conv1D(channel, kernel_size=3, activation='relu'),
-        MaxPooling3=tf.keras.layers.MaxPooling1D(4),
-        Conv4=tf.keras.layers.Conv1D(channel, kernel_size=3, activation='relu'),
-        MaxPooling4=tf.keras.layers.MaxPooling1D(4),
-        Conv5=tf.keras.layers.Conv1D(channel, kernel_size=3, activation='relu'),
-        MaxPooling5=tf.keras.layers.MaxPooling1D(4),
-        Flatten=tf.keras.layers.Flatten(),
-        Dense=tf.keras.layers.Dense(output, activation='softmax')
+    
+    def create_model():
+        model = M4
+        print(model)
         
-    def call(self, x):
-        x = tf.layers.flatten(x)
-        x = self.Conv1(x)
-        x = self.MaxPooling1(x)
-        x = self.Conv2(x)
-        x = self.MaxPooling2(x)
-        x = self.Conv3(x)
-        x = self.MaxPooling3(x)
-        x = self.Conv4(x)
-        x = self.MaxPooling4(x)
-        x = self.Conv5(x)
-        x = self.MaxPooling5(x)
-        x = self.Flatten(x)
-        x = self.Dense(x)
-        return x
-
-        # return model
-
     def load_model():
         model_friends = tf.keras.models.load_model("/content/drive/MyDrive/Audio/audio_friends.h5")
         return(model_friends)
-
+if __name__ == "__main__":
+    audio_classify = Audio_Classifier()
+    audio_classify.create_model()
