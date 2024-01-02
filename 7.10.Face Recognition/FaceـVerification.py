e09e765a8ac7fed52edb470804cd2db8ba4484d2
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-01-02 06:37:34 +0330

    Add  7.10.Face Recognition

---
+++ 7.10.Face Recognition/FaceـVerification.py
@@ -0,0 +1,32 @@
+import argparse
+import cv2
+import numpy as np
+from insightface.app import FaceAnalysis
+
+# __Argument Parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--image1', type=str , help="Taking the first photo")
+parser.add_argument('--image2', type=str , help="Taking a second photo")
+opt = parser.parse_args()
+
+# Create FaceAnalysis
+app = FaceAnalysis(name="buffalo_s",providers=['CPUExecutionProvider'])
+app.prepare(ctx_id=0, det_size=(640, 640))
+
+# Catch yore imags
+imag_1 = cv2.imread(opt.image1)
+imag_1 = cv2.cvtColor(imag_1,cv2.COLOR_BGR2RGB)
+imag_2 = cv2.imread(opt.image2)
+imag_2 = cv2.cvtColor(imag_2,cv2.COLOR_BGR2RGB)
+
+# embeddings
+result_1 = app.get(imag_1)
+embedding_1 = result_1[0]["embedding"].shape
+result_2 = app.get(imag_2)
+embedding_2 = result_2[0]["embedding"].shape
+
+# Result
+if np.sqrt(np.sum((embedding_1 - embedding_2) **2))<25:
+    print("Same Person ✅ ")
+else:
+    print("Different Persons 📛")    
---
+++ 7.10.Face Recognition/README.md
@@ -0,0 +1,12 @@
+# Face Recognition 🧑
+In this face recognition project using InsightFace
+ And pytorch is built 🧑
+# How to run
+```
+pip install -r requirements.txt
+```
+## Inference:
+```
+python3 face_verification.py --image1 {yore imag1} --image2 {yore imag2}
+```
+
