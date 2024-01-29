80b6c036220075d7c821fd2b335d440c9819251c
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-01-29 22:59:31 +0330

    Update file is Classifer Emogi Text

--- 7.11Text Classifcation/ClassifierEmojiText.py
+++ 7.11Text Classifcation/ClassifierEmojiText.py
@@ -10,8 +10,8 @@ class EmojiTextClassifier():
             self. X_train , self.Y_train = read_csv("/content/drive/MyDrive/dataset/Emoji_Text_Classification/train.csv")
             self.X_test , self.Y_test = read_csv("/content/drive/MyDrive/dataset/Emoji_Text_Classification/test.csv")
           
-    def load_feature_vectors(self):
-        self.f = open("/content/drive/MyDrive/dataset/glov.6b/glove.6B.50d.txt", encoding="utf_8")
+    def load_feature_vectors(self,hrf):
+        self.f = open(hrf, encoding="utf_8")
         self.word_vectors = {}
         for line in self.f:
             line = line.strip().split()
@@ -20,19 +20,18 @@ class EmojiTextClassifier():
             self.word_vectors[word] = vector
         return self.word_vectors
 
-    def sentence_to_feature_vectors_avg(self,sentence):
-              self.sentence = sentence.lower()
-              words = self.sentence.strip().split(' ')
-              sum_vectors = np.zeros((50,))
-              for word in words:
-                  sum_vectors += self.word_vectors[word]
+    def sentence_to_feature_vectors_avg(self,sentence,veger_number):
+          self.sentence = sentence.lower()
+          self.words = sentence.strip().split(" ")
+          self.sum_vectors = np.zeros((veger_number, ))
+          for word in self.words:
+            self.sum_vectors += self.word_vectors[word]
+            
+          self.avg_vectors = self.sum_vectors / len(self.words)
+          return self.avg_vectors
 
-              avg_words = sum_vectors / len(words)
-
-              return avg_words
-
-    def load_model(self,avg_vectors=None): 
-        self.model = tf.keras.models.load_model("/content/drive/MyDrive/Emoji_text_classiftcation.h5")
+    def load_model(self,hrf_model,avg_vectors=None): 
+        self.model = tf.keras.models.load_model(hrf_model)
         return self.model
 
     def label_to_emoji(self,label):
@@ -41,23 +40,16 @@ class EmojiTextClassifier():
 
         return emojies[self.label]
 
-    def predict(self ,my_test):
+    def predict(self ,my_test,veger_number):
         self.sentence_test = my_test
-        self.my_test_avg = self.sentence_to_feature_vectors_avg(self.sentence_test)
+        self.my_test_avg = self.sentence_to_feature_vectors_avg(self.sentence_test,veger_number)
         self.my_test_avg = np.array([self.my_test_avg])
         self.result = self.model.predict(self.my_test_avg)
         self.y_pred = np.argmax(self.result)
         emojie = self.label_to_emoji(self.y_pred)
         print(emojie)
+        # return self.label_to_emoji(y_pred)
 
-
-
-def senteence_to_avg(sentence):
-    sentence = sentence.lower()
-    words = sentence.strip().split(' ')
-    sum_vectors = np.zeros((50,))
-    for word in words:
-        sum_vectors += word_vectors[word]
-    avg_words = sum_vectors / len(words)
-
-    return avg_words
+    def test(self,test=True):
+        result = self.model.evaluate(self.X_test, self.Y_test)    
+        return result
